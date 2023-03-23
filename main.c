508209edef07df8fa2893327303d955030a1b4d5
Giovanni Paredes <107645957+Gigio2077@users.noreply.github.com>
2023-03-23 15:53:40 -0300

    fisica melhorada com gravidade

--- main.c
+++ main.c
@@ -4,55 +4,38 @@
 #include <time.h>
 #include <math.h>
 
-#define SPRITESIZE 32
-#define ALTURA 480
+#define SPRITESIZE 16
+#define ALTURA 530
 #define LARGURA 960
 #define TILE 16
-
-
+#define SPEED 4
+#define GRAVIDADE 0.2
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
 
-
-
-
-
-//função haveraColisao(Bloco, Player,int delta x, delta y, sentido){}
-int haveraColisao (Rectangle player, Rectangle parede, int delta, int a){
-
-    switch(a){
-
+int haveraColisao (Rectangle player, Rectangle parede, int delta, int direcao){
+    switch(direcao){
         case 1://ESQUERDA
-
             player.x-=delta;
             if(CheckCollisionRecs(parede, player)){return 1;}
             else{return 0;}
             break;
-
-
         case 2://CIMA
-
             player.y-=delta;
             if(CheckCollisionRecs(parede, player)){return 1;}
             else{return 0;}
             break;
-
         case 3:// DIREITA
-
             player.x+=delta;
             if(CheckCollisionRecs(parede, player)){return 1;}
             else{return 0;}
             break;
-
         case 4:// BAIXO
-
             player.y+=delta;
             if(CheckCollisionRecs(parede, player)){return 1;}
             else{return 0;}
             break;
-
-
         default:
             return 0;
         }
@@ -60,53 +43,66 @@ int haveraColisao (Rectangle player, Rectangle parede, int delta, int a){
 
 }
 
+void gravidade(int *posY, float *g, int R, int C,int pulou)
+{
+/*R refere a Repouso de main() */
+    if(R){return;}
+    if (!(R)&&pulou)//não está em repouso e pulou
+    {
+        //g é a velocidade com que a gravidade aumenta
+        *g+=GRAVIDADE;
+        *posY-=SPEED*2;
+        *posY+=*g;
+
+        //usando função haveracolisão da main()
+    }
+
+    if(!(R)&&!pulou)//não está em repouso e não pulou(queda livre ou bateu no teto)
+    {
+        *g+=GRAVIDADE;
+        *posY+=*g;
+    }
 
 
 
-/*modificar função movimento
-para que ela permita o movimento desde que haveraColisao seja falso
-haveraColisao ( player, blocosProximos);
-
-
-*/
-
-void movimento(int *posX, int *posY, int E, int D, int S, int R){
-
-    if((IsKeyDown(KEY_LEFT)) && !E )    {*posX-=4;}
-    if((IsKeyDown(KEY_UP)) && !S )      {*posY-=15;}
-    if( (IsKeyDown(KEY_RIGHT)) && !D )    {*posX+=4;}
-    if ( !R)    {*posY+=5;}
 
 }
 
+void movimento(int *posX, int *posY, int E, int D, int S, int *R, int *pulou){
 
+    if((IsKeyDown(KEY_LEFT)) && !E )    {*posX-=SPEED;}
+    if((IsKeyDown(KEY_UP)) && !S && *R )      {*R = 0;*pulou=1;}
+    if( (IsKeyDown(KEY_RIGHT)) && !D )    {*posX+=SPEED;}
 
-
-
-
+}
 
 int main()
 {
-    int colisaoSuperior = 0;
-    int colisaoDireita = 0;
-    int colisaoEsquerda = 0;
-    int Repouso = 0;
+
+    int colisaoSuperior,colisaoDireita,colisaoEsquerda,Repouso,i = 0;
     int index;
+    int ajuste,pulou = 0;
     SetRandomSeed(time(NULL));
     float g = 0;
-    int i= 0;
     int qtdBlocosGrama = 0;
     int qtdBlocosTerra = 0;
-
-    int posX = 450;
-    int posY = 450;
     char mapa[30][61];
+
+    //-------------------------
+    //recebera o "J" da função carrega mapa
+    int posX = 416;
+    int posY = 288;
+    //-------------------------
 
     float update = 0;
 
 
 
 
+
+
+//--------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------
 //carrega mapa
         FILE *fp;
@@ -122,8 +118,6 @@ int main()
             }
         }
         fclose(fp);
-
-
     char currentchar;
     char hashatag = '#';
     char T = 'T';
@@ -134,9 +128,7 @@ int main()
 
             if( currentchar==hashatag ){
                     qtdBlocosGrama++;
-
             }
-
         }
     }
      for( int i=0; i<30; i++){
@@ -145,46 +137,34 @@ int main()
 
             if( currentchar==T ){
                     qtdBlocosTerra++;
-
             }
-
         }
     }
     printf("\n\n\n%d\n\n",qtdBlocosGrama);
-
 Rectangle BlocoTerra[qtdBlocosTerra];
 index = 0;
     for( int i=0; i<30; i++){
         for( int j=0; j<61; j++){
             currentchar = mapa[i][j];
-
             if( currentchar==T ){
-
-
         BlocoTerra[index].width = TILE ;
         BlocoTerra[index].height = TILE ;
         BlocoTerra[index].x = TILE*j ;
         BlocoTerra[index].y = TILE*i ;
         index++;
-
     }}}
 index = 0;
-
 Rectangle BlocoGrama[qtdBlocosGrama];
 index = 0;
     for( int i=0; i<30; i++){
         for( int j=0; j<61; j++){
             currentchar = mapa[i][j];
-
             if( currentchar==hashatag ){
-
-
         BlocoGrama[index].width = TILE ;
         BlocoGrama[index].height = TILE ;
         BlocoGrama[index].x = TILE*j ;
         BlocoGrama[index].y = TILE*i ;
         index++;
-
     }}}
 index = 0;
 
@@ -193,45 +173,34 @@ index = 0;
 //--------------------------------------------------------------------------------------
 
 
-
     // Initialization
     //--------------------------------------------------------------------------------------
-
-
-
-
-
-    InitWindow(LARGURA, ALTURA, "Tua mae ");
-
+    InitWindow(LARGURA, ALTURA, "Jogo Raylib  ");
 
 //----------------------------------------------------------------------------------
 //TEXTURAS
-    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
+    // NOTE: Texturas DEVEM ser carregadas depois da Window initialization (OpenGL context is required)
 
-
-    Texture2D BG3 = LoadTexture("texturas/BG3.png");        // Texture loading
+    Texture2D BG3 = LoadTexture("texturas/BG3.png");        // Carregando Texturas
     Texture2D BG1 = LoadTexture("texturas/BG1.png");
     Texture2D grama = LoadTexture("texturas/grama.png");
     Texture2D terra = LoadTexture("texturas/terra.png");
 
-
 //----------------------------------------------------------------------------------
-
-
     SetTargetFPS(60);
-
 //--------------------------------------------------------------------------------------
 
 // LOOP PRINCIPAL DO JOGO
-
-
-
 
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        // TODO: Atualiza as variáveis aqui
+            colisaoSuperior = 0;
+            colisaoDireita = 0;
+            colisaoEsquerda = 0;
+
         //----------------------------------------------------------------------------------
 
 // DESENHO
@@ -245,37 +214,17 @@ index = 0;
             DrawTexture(BG1, 0, 0, WHITE);
             DrawTexture(BG3, 0, 0, WHITE);
         //----------------------------------------------------------------------------------
-
-
             Rectangle player = {(double)posX, (double) posY, SPRITESIZE, SPRITESIZE};
+            /*
+            //printa posição
             //printf("posicao x :%f e y:%f \n",player.x,player.y);
-
-            Rectangle bordaSuperior = {0,  0, LARGURA, 1};
-            Rectangle bordaEsquerda = {1,  1, 1, ALTURA};
-            Rectangle bordaInferior = {1,  ALTURA-1, LARGURA, 1};
-            Rectangle bordaDireita = {LARGURA-1,  1, 1,ALTURA};
-
-
-            DrawRectangleRec(bordaSuperior,RED);
-            DrawRectangleRec(bordaEsquerda,RED);
-            DrawRectangleRec(bordaInferior,RED);
-            DrawRectangleRec(bordaDireita,RED);
-
-           // printf("%d \n", haveraColisao(bordaSuperior,player,5,5,2));
-
-
-           //printf("%d \n", haveraColisao(player,BlocoTerra[3],50,3));
-
-          // printf("%d %d \n", (int)player.x/16, (int)player.y/16);
-
-
-
-            //DrawTexture(BG1, 0, 0, WHITE);
-           // DrawTexture(BG3, 0, 0, WHITE);
-
-            DrawRectangleRec( player, RED);                        // Draw a color-filled rectangle
-
-
+            //printa coordenadas
+            //printf("%d %d \n", (int)player.x/16, (int)player.y/16);
+            */
+            //aqui será substituido por uma função com DrawTexture() periódico para adicionar texturas ao player, moeda e inimigos
+            DrawRectangleRec( player, RED);
+            //------------------------------------------------------------------------------
+            // sera substituido por uma funçao que desenha todos blocos sem animacao .
             for(int k = 0; k<qtdBlocosGrama; k++){
                 DrawTexture(grama,
                             BlocoGrama[k].x,
@@ -289,143 +238,64 @@ index = 0;
                                         BlocoTerra[k].y,
                                         WHITE);
                                         }
+            //------------------------------------------------------------------------------
 
 
 
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//loop colisoes
+    for (i = 0; i<qtdBlocosGrama; i++)
+{
+                colisaoDireita += haveraColisao(player,BlocoGrama[i],SPEED,3);
+                colisaoEsquerda += haveraColisao(player,BlocoGrama[i],SPEED,1);
+                colisaoSuperior+= haveraColisao(player,BlocoGrama[i],abs(SPEED*2-(g+GRAVIDADE)),2);
+    //colisão com o teto
+            if(haveraColisao(player,BlocoGrama[i],abs(SPEED*2-(g+GRAVIDADE)),2)&&!Repouso)
+                {
+                ajuste = player.y-(BlocoGrama[i].y+BlocoGrama[i].height) ;
+                //posY-=ajuste;
+                printf("\najuste teto");
+                ajuste = 0;
+                pulou=0;
+                Repouso = 0;
+                break;
+                }
 
+                Repouso+= haveraColisao(player,BlocoGrama[i],g+GRAVIDADE,4);
 
+    //colisão com o solo
+            if(haveraColisao(player,BlocoGrama[i],g+GRAVIDADE,4))
+                {
+                ajuste = BlocoGrama[i].y - (player.y+player.height) ;
+                printf("R: %d\n",Repouso );
+                posY+=ajuste;
+                ajuste = 0;
+                pulou = 0;
+                g = 0;
 
+                break;
+                }
 
-int delta = 0;
-
-
-
-for (i = 0; i<qtdBlocosGrama; i++){
-
-        /*
-        remover tantas variaveis, tentar passar o numero da direcao apenas
-         retornar distancia precisa para que quase encoste
-
-        */
-        if(haveraColisao(player,BlocoGrama[i],5,3))
-        {
-            delta = BlocoGrama[i].x - player.x-player.width-1;
-            printf("delta %d\n",delta );
-
-        }
-
-
-        colisaoDireita += haveraColisao(player,BlocoGrama[i],5,3);
-
-
-
-
-        if(haveraColisao(player,BlocoGrama[i],5,1))
-        {
-            delta = player.x-BlocoGrama[i].x -BlocoGrama[i].width-1 ;
-            printf("delta %d\n",delta );
-
-        }
-        colisaoEsquerda += haveraColisao(player,BlocoGrama[i],5,1);
-
-
-
-
-        if(haveraColisao(player,BlocoGrama[i],5,2))
-        {
-            delta = player.y-BlocoGrama[i].y -BlocoGrama[i].height+1 ;
-            printf("delta %d\n",delta );
-
-        }
-
-        colisaoSuperior += haveraColisao(player,BlocoGrama[i],5,2);
-
-
-
-
-        //mudar o 10 para g posteriormente
-        if(haveraColisao(player,BlocoGrama[i],5,2))
-        {
-            delta = BlocoGrama[i].y -BlocoGrama[i].height+1 ;
-            printf("delta %d\n",delta );
-
-        }
-        Repouso += haveraColisao(player,BlocoGrama[i],5,4);
 
 }
-
-movimento(&posX, &posY, colisaoEsquerda ,colisaoDireita,colisaoSuperior, Repouso);
-
-//printf("%d",Repouso);
-
-            printf("delta %d\n",delta );
-
-
-colisaoSuperior = 0;
-colisaoDireita = 0;
-colisaoEsquerda = 0;
-//Repouso = 0;
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 
 
 
-
-
-
-
-
-
-
-
-
+movimento(&posX, &posY, colisaoEsquerda ,colisaoDireita,colisaoSuperior, &Repouso,&pulou);
+gravidade(&posY,&g,Repouso,colisaoSuperior,pulou);
 Repouso = 0;
-
-
-
-
-//----------------------------------------------------------------------------------
-//MOVIMENTO
-/*
-        if(IsKeyDown(KEY_LEFT)&& (podemover(1,&posX,&posY)) ){posX-=5;}
-        if(IsKeyPressed(KEY_UP)&& (podemover(2,&posX,&posY)) && (g==0)) {posY-=20;}
-        if(IsKeyDown(KEY_RIGHT)&& (podemover(3,&posX,&posY)) ){posX+=5;}
-        if(IsKeyDown(KEY_DOWN)&& (podemover(4,&posX,&posY)) ) {posY+=5;}
-
-//----------------------------------------------------------------------------------
-// RELOGIO  --ajustar o segundo
-
-        update += (GetFrameTime()*4);
-        i = (int)update;
-        if (update>=4){update = 0;}
-*/
-//----------------------------------------------------------------------------------
-//GRAVIDADE
-
-
-
-
-
-
-
-//----------------------------------------------------------------------------------
-
-        //colisao_parede(parede,player);
-
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
-
-
-
-
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
-
-
-
 
     return 0;
     }
