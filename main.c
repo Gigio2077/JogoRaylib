b27b134fff8a09fcb86a14fab27d503d1317ca2d
Giovanni Paredes <107645957+Gigio2077@users.noreply.github.com>
2023-03-31 13:02:54 -0300

    Menu e saves

--- main.c
+++ main.c
@@ -12,10 +12,8 @@
 #define SPEED 4
 #define GRAVIDADE 0.2
 
+bool Paused = true;
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 
 typedef struct texturas{
 
@@ -26,6 +24,7 @@ typedef struct texturas{
     Texture2D frame;
     Texture2D trap ;
     Texture2D coin;
+    //JOGADOR
     Texture2D spriterunright ;
     Texture2D spriterunleft;
     Texture2D coin_grande;
@@ -43,6 +42,8 @@ typedef struct texturas{
     Texture2D fallleft;
     Texture2D ataqueright;
     Texture2D ataqueleft;
+    //TECLAS
+    Texture2D C,Esc,N,Q,S,V,um,dois,tres;
 
 
 
@@ -53,33 +54,30 @@ typedef struct texturas{
 
 }TEXTURAS;
 
+typedef struct save {
+    int fase,vidas,score,trocarmapa,menustate;
+} SAVE;
+
 typedef struct entidade{
 
-        int frame,currentframe,Repouso,Pulou,ColisaoEsq,ColisaoDir,ColisaoSup;
-        int fase,spawnX,spawnY;
-        int trocarmapa,direita;
+        //variaveis da fisica
+        int Repouso, Pulou, ColisaoEsq,ColisaoDir,ColisaoSup;
+        int spawnX,spawnY;
         Rectangle retangulo;
+        float g;
+
+        //variaveis animacoes
+        int frame,currentframe;
+        int direita;
         Texture2D texturaatual;
         Rectangle framerec;
         Rectangle framerecportal;
+
+        //variavies funcionais
+        int fase;
         Rectangle ataque[5];
         int atacando;
-        float g;
-        char Estado[10];
-        /*
 
-        J - Jump
-        R - run right
-        r - idle right
-        L - run left
-        l - idle left
-        a - attack1
-        A - Attack2
-        f - fall
-        m - mortal
-        s - estático
-
-        */
     }ENTIDADE;
 
 typedef struct bloco_tipo{
@@ -101,14 +99,18 @@ typedef struct mapalido{
     int posinimigos[15][1][1];
     }MAPA;
 
-void hud (MAPA *mapalido,ENTIDADE *player,TEXTURAS texturas){
+void HUD (MAPA *mapalido,ENTIDADE *player,TEXTURAS texturas,SAVE *saveatual){
+
 //rawRectangle(int posX, int posY, int width, int height, Color color);
 DrawRectangle(0,480,960,50,DARKGRAY);
+
 DrawText((TextFormat("x %d", mapalido->qtdCoins)), 80, 490, 35, MAROON);
 DrawTexture(texturas.coin_grande,0,473,WHITE);
-DrawText((TextFormat("x %d", mapalido->qtdCoins)), 250, 490, 35, MAROON);
+
+DrawText((TextFormat("x %d", saveatual->vidas)), 250, 490, 35, MAROON);
 DrawTexture(texturas.heart,170,473,WHITE);
-DrawText((TextFormat("FASE: %d", player->fase)), 450, 490, 35, MAROON);
+
+DrawText((TextFormat("FASE: %d", saveatual->fase)), 450, 490, 35, MAROON);
 
 DrawText((TextFormat("fps: %d", GetFPS())), 840, 490, 35, MAROON);
 
@@ -116,10 +118,196 @@ DrawText((TextFormat("fps: %d", GetFPS())), 840, 490, 35, MAROON);
 }
 
 
+
+//FUNCOES MENU
+void salva_save(const SAVE *save,  char nomearquivo[]) {
+    FILE *fp;
+    fp = fopen(nomearquivo, "wb"); //abrir arquivo modo escrita binario
+    if (fp == NULL) {
+        printf("falha ao abrir arquivo.\n");
+        return;
+    }
+    fwrite(save, sizeof(SAVE), 1, fp);
+    fclose(fp);
+}
+
+void carrega_save(SAVE *save, char filename[]) {
+    FILE *fp;
+    fp = fopen(filename, "rb");
+        if (fp == NULL) {
+            printf("Save nao encontrado\n");
+            return;
+        }
+
+    fread(save, sizeof(SAVE), 1, fp);
+    fclose(fp);
+}
+
+void GUI(TEXTURAS texturas,SAVE *saveatual,SAVE *save1,SAVE *save2,SAVE *save3,ENTIDADE*player){
+
+
+        if(IsKeyPressed(KEY_V)&&saveatual->menustate==1){Paused = !Paused;}
+
+        if(IsKeyPressed(KEY_V)){
+                    saveatual->menustate=1;
+                }
+
+
+        if(IsKeyPressed(KEY_N)){
+                    saveatual->fase=1;
+                    saveatual->trocarmapa=1;
+                    saveatual->vidas=2;
+                    Paused = !Paused;
+                }
+        if(IsKeyPressed(KEY_C)){
+                    saveatual->menustate=2;
+                }
+        if(IsKeyPressed(KEY_S)){
+                    saveatual->menustate=3;
+                }
+
+
+
+
+
+
+
+       if(IsKeyPressed(KEY_KP_1)||IsKeyPressed(KEY_ONE)){
+        if(saveatual->menustate==2){
+            carrega_save(saveatual,"save1.bin");
+            Paused = !Paused;
+            saveatual->trocarmapa = 1;
+            respawna(&player,1);
+        }
+        if(saveatual->menustate==3){
+            salva_save(saveatual,"save1.bin");
+            //carrega_save(saveatual,"save1.bin");
+
+        }
+    }
+
+    if(IsKeyPressed(KEY_KP_2)||IsKeyPressed(KEY_TWO)){
+        if(saveatual->menustate==2){
+            carrega_save(saveatual,"save2.bin");
+            Paused = !Paused;
+            saveatual->trocarmapa = 1;
+            respawna(&player,1);
+        }
+        if(saveatual->menustate==3){
+            salva_save(saveatual,"save2.bin");
+            //carrega_save(saveatual,"save1.bin");
+
+        }
+    }
+
+    if(IsKeyPressed(KEY_KP_3)||IsKeyPressed(KEY_THREE)){
+        if(saveatual->menustate==2){
+            carrega_save(saveatual,"save3.bin");
+            Paused = !Paused;
+            saveatual->trocarmapa = 1;
+            respawna(&player,1);
+        }
+        if(saveatual->menustate==3){
+            salva_save(saveatual,"save3.bin");
+        }
+    }
+
+
+
+
+        switch (saveatual->menustate){
+
+            case 1:
+                DrawRectangle(LARGURA/3, 50, LARGURA/3, 450 ,LIGHTGRAY);
+                DrawText("Menu",405,100,60,MAROON);
+
+                DrawTexture(texturas.N,358,185,WHITE);
+                DrawText("ovo jogo",390,180,36,MAROON);
+
+                DrawTexture(texturas.C,358,225,WHITE);
+                DrawText("arregar jogo",390,220,36,MAROON);
+
+                DrawTexture(texturas.S,358,265,WHITE);
+                DrawText("alvar jogo",390,260,36,MAROON);
+
+                DrawTexture(texturas.V,358,305,WHITE);
+                DrawText("oltar",390,300,36,MAROON);
+
+                DrawTexture(texturas.Q,358,425,WHITE);
+                DrawText("uit",390,420,36,MAROON);
+
+
+            break;
+
+
+            case 2:
+                DrawRectangle(LARGURA/3, 50, LARGURA/3, 450 ,LIGHTGRAY);
+                DrawText("Menu",405,100,60,MAROON);
+
+                DrawText("Carregar save:",370,180,36,MAROON);
+
+
+                DrawTexture(texturas.um,358,225,WHITE);
+                DrawText("slot1",390,220,36,MAROON);
+                DrawText((TextFormat("vidas:%d\tfase:%d", save1->vidas,save1->fase)),485,230,16,MAROON);
+
+
+
+                DrawTexture(texturas.dois,358,265,WHITE);
+                DrawText("slot2",390,260,36,MAROON);
+                DrawText((TextFormat("vidas:%d\tfase:%d", save1->vidas,save2->fase)),485,270,16,MAROON);
+
+
+                DrawTexture(texturas.tres,358,305,WHITE);
+                DrawText("slot3",390,300,36,MAROON);
+                DrawText((TextFormat("vidas:%d\tfase:%d", save1->vidas,save3->fase)),485,310,16,MAROON);
+
+
+                DrawTexture(texturas.V,358,425,WHITE);
+                DrawText("voltar",390,420,36,MAROON);
+
+            break;
+
+
+            case 3:
+                DrawRectangle(LARGURA/3, 50, LARGURA/3, 450 ,LIGHTGRAY);
+                DrawText("Menu",405,100,60,MAROON);
+
+                DrawText("Salvar save:",370,180,36,MAROON);
+
+
+                DrawTexture(texturas.um,358,225,WHITE);
+                DrawText("slot1",390,220,36,MAROON);
+                DrawText((TextFormat("vidas:%d\tfase:%d", save1->vidas,save1->fase)),485,230,16,MAROON);
+
+
+
+                DrawTexture(texturas.dois,358,265,WHITE);
+                DrawText("slot2",390,260,36,MAROON);
+                DrawText((TextFormat("vidas:%d\tfase:%d", save1->vidas,save2->fase)),485,270,16,MAROON);
+
+
+                DrawTexture(texturas.tres,358,305,WHITE);
+                DrawText("slot3",390,300,36,MAROON);
+                DrawText((TextFormat("vidas:%d\tfase:%d", save1->vidas,save3->fase)),485,310,16,MAROON);
+
+
+                DrawTexture(texturas.V,358,425,WHITE);
+                DrawText("voltar",390,420,36,MAROON);
+
+            break;
+
+
+        }
+
+
+
+}
+
+
+
 //FUNCOES MAPA
 void leMapa(char nomemapa[], MAPA *mapalido, ENTIDADE *player){
-
-
 
     FILE *fp;
     fp = fopen(nomemapa, "r");
@@ -162,6 +350,7 @@ void leMapa(char nomemapa[], MAPA *mapalido, ENTIDADE *player){
                         player->ColisaoSup = 0;
                         player->Pulou = 0;
                         player->direita = 1;
+                        player->frame = 0;
                         for(int z = 0;z<5;z++){
                         player->ataque[z].x=-100;}
 
@@ -246,19 +435,16 @@ void leMapa(char nomemapa[], MAPA *mapalido, ENTIDADE *player){
 
     }
 }
-void init_nome_mapas(char nome_mapas[][18]) {
-    char aux[18] = "mapas/";
-    int i;
-    for (i = 0; i < 7; i++) {
-        sprintf(nome_mapas[i], "mapa%d.txt", i + 1);
-        strcat(aux, nome_mapas[i]);
-        strcpy(nome_mapas[i], aux);
-        strcpy(aux, "mapas/");
-        }
 
-    for (i = 0; i < 7; i++) {
-        printf("O valor de mapa[%d] is: %s\n", i, nome_mapas[i]);
-        }
+void init_nome_mapas(char nome_mapas[][18]) {
+    for(int i = 1; i<10; i++){
+        sprintf(nome_mapas[i], "mapas/mapa%d.txt", i);
+    }
+
+    for(int i = 1; i<10; i++){
+        printf("\nA string em nome_mapas[%d] eh:%s",i,nome_mapas[i]);
+
+    }
 }
 
 void renderiza_mapa(MAPA *mapalido,ENTIDADE *player,TEXTURAS texturas){
@@ -311,6 +497,9 @@ void renderiza_mapa(MAPA *mapalido,ENTIDADE *player,TEXTURAS texturas){
 
 
     }
+
+
+
 //--------------------------------------------------------------------------------------
 
 //FUNCOES PLAYER
@@ -318,13 +507,11 @@ void renderiza_mapa(MAPA *mapalido,ENTIDADE *player,TEXTURAS texturas){
 //--------------------------------------------------------------------------------------
 
 void atualizaPlayer(ENTIDADE *player){
-//atualiza sprites e estados para default
+//atualiza frameccounter e estados para default
         player->ColisaoDir = 0;
         player->ColisaoEsq = 0;
         player->ColisaoSup = 0;
         player->Repouso = 0;
-
-
         player->frame++;
 
     if (player->frame >= (8))
@@ -343,7 +530,7 @@ void atualizaPlayer(ENTIDADE *player){
 void spritesPlayer(ENTIDADE *player,TEXTURAS texturas){
 
     Texture2D textura_atual;
-    Vector2 position_player = {player->retangulo.x,player->retangulo.y};
+    Vector2 position_player = {player->retangulo.x+4,player->retangulo.y+4};
     if(!IsKeyDown(KEY_LEFT)&&!(IsKeyDown(KEY_RIGHT)&&player->Repouso)){
             if(player->direita){textura_atual=texturas.idleright;}
             else{textura_atual=texturas.idleleft;}
@@ -418,6 +605,7 @@ int haveraColisao (Rectangle player, Rectangle parede, int delta, int direcao){
 
 void movimento(ENTIDADE *player) {
 
+
     if ((IsKeyDown(KEY_LEFT)) && !(player->ColisaoEsq))
         {
             player->retangulo.x -= SPEED;
@@ -470,7 +658,7 @@ void gravidade(ENTIDADE *player) {
 
 }
 
-void checacolisoes(MAPA *mapalido,ENTIDADE *player){
+void checacolisoes(MAPA *mapalido, ENTIDADE *player, SAVE *saveatual){
 
     float ajuste = 0;
 
@@ -482,10 +670,11 @@ void checacolisoes(MAPA *mapalido,ENTIDADE *player){
             mapalido->qtdCoins--;
             }
     }
-
+//morrer espinhos
     for(int k = 0; k<mapalido->qtdArmadilhas; k++){
         if(CheckCollisionRecs(mapalido->Armadilhas[k], player->retangulo))
             {
+            saveatual->vidas--;
             player->g=0;
             player->Repouso=1;
             respawna(player,1);
@@ -495,8 +684,9 @@ void checacolisoes(MAPA *mapalido,ENTIDADE *player){
 //portal próxima fase
     if(CheckCollisionRecs(mapalido->portal, player->retangulo))
         {
-        player->fase++;
-        player->trocarmapa = 1;
+        saveatual->fase++;
+        saveatual->vidas++;
+        saveatual->trocarmapa = 1;
         }
 
     for (int i = 0; i<mapalido->qtdBlocosGrama; i++)
@@ -506,14 +696,14 @@ void checacolisoes(MAPA *mapalido,ENTIDADE *player){
             player->ColisaoSup+= haveraColisao(player->retangulo,mapalido->BlocoGrama[i],SPEED*2-(player->g+GRAVIDADE),2);
             player->Repouso+= haveraColisao(player->retangulo,mapalido->BlocoGrama[i],player->g+GRAVIDADE,4);
 
-        //colisão com o teto
+//colisão com o teto
             if(haveraColisao(player->retangulo,mapalido->BlocoGrama[i],abs(SPEED*2-(player->g+GRAVIDADE)),2))
                 {
                     if(player->g>4){player->g=0;}
 
                     player->Pulou = 0;
                 }
-        //colisão com o solo
+//colisão com o solo
             if(haveraColisao(player->retangulo,mapalido->BlocoGrama[i],player->g+GRAVIDADE,4))
                 {
                 ajuste = mapalido->BlocoGrama[i].y - (player->retangulo.y+player->retangulo.height) ;
@@ -525,9 +715,8 @@ void checacolisoes(MAPA *mapalido,ENTIDADE *player){
                 }
     }
 }
-/*
-void ataques(ENTIDADE *player, TEXTURAS texturas){
 
+/*void ataques(ENTIDADE *player, TEXTURAS texturas){
 
     if(IsKeyDown(KEY_Q)){
             player->atacando=1;
@@ -547,32 +736,42 @@ void ataques(ENTIDADE *player, TEXTURAS texturas){
 */
 
 
-
 //--------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------
 // MAIN()
-bool Paused = false;
+
+
     int main(){
             SetRandomSeed(time(NULL));
-            char nome_mapas[7][18];
+            char nome_mapas[10][18];
             init_nome_mapas(nome_mapas);
+
             Rectangle player_frame_rect = {0, 0, SPRITESIZE, SPRITESIZE};
+
             TEXTURAS texturas;
             ENTIDADE player;
-            player.frame = 0;
-            player.fase = 1;
-            player.trocarmapa = 0;
-            MAPA mapas[7];
-
-    leMapa(nome_mapas[0], &mapas[0], &player);
-    Rectangle player_rect = {((double)player.spawnX), ((int)player.spawnY), SPRITESIZE, SPRITESIZE};
-    player.framerecportal = player_frame_rect;
-    player.framerecportal.width = 16;
+                player.frame = 0;
+                player.retangulo.width=SPRITESIZE;
+                player.retangulo.height=SPRITESIZE;
+            MAPA mapas[10];
+            SAVE save1;SAVE save2; SAVE save3;
 
 
-    player.framerec = player_frame_rect;
-    player.retangulo = player_rect;
+
+            SAVE saveatual;
+                        saveatual.trocarmapa = 0;
+                        saveatual.fase=0;
+                        saveatual.menustate = 1;
+
+
+    //leMapa(nome_mapas[1], &mapas[1], &player);
+
+    //Rectangle player_rect = {((double)player.spawnX), ((int)player.spawnY), SPRITESIZE, SPRITESIZE};
+            player.framerecportal = player_frame_rect;
+            player.framerecportal.width = 16;
+            player.framerec = player_frame_rect;
+    //player.retangulo = player_rect;
 
 //--------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------
@@ -598,7 +797,7 @@ bool Paused = false;
         texturas.portal= LoadTexture("texturas/portal.png");
 
 
-        //texturas player
+        //TEXTURAS PLAYER
         texturas.idleright = LoadTexture("texturas/idleright.png");
         texturas.idleleft = LoadTexture("texturas/idleleft.png");
         texturas.runright = LoadTexture("texturas/runright.png");
@@ -612,6 +811,17 @@ bool Paused = false;
         texturas.ataqueright = LoadTexture("texturas/ataqueright.png");
         texturas.ataqueleft = LoadTexture("texturas/ataqueleft.png");
 
+        //TEXTURAS TECLAS
+        texturas.C = LoadTexture("texturas/C-Key.png");
+        texturas.Esc = LoadTexture("texturas/Esc-Key.png");
+        texturas.N = LoadTexture("texturas/N-Key.png");
+        texturas.Q = LoadTexture("texturas/Q-Key.png");
+        texturas.S = LoadTexture("texturas/S-Key.png");
+        texturas.V = LoadTexture("texturas/V-Key.png");
+
+        texturas.um = LoadTexture("texturas/um.png");
+        texturas.dois = LoadTexture("texturas/dois.png");
+        texturas.tres = LoadTexture("texturas/tres.png");
 
 
 
@@ -623,37 +833,72 @@ bool Paused = false;
 
     while (!WindowShouldClose())
     {
-        if(IsKeyPressed(KEY_P))
-            Paused = !Paused;
+
+
+        carrega_save(&save1,"save1.bin");
+        carrega_save(&save2,"save2.bin");
+        carrega_save(&save3,"save3.bin");
+
+        if(IsKeyPressed(KEY_ESCAPE)&& Paused==false) {
+                Paused = !Paused;
+                saveatual.menustate = 1;
+        }
+
 
 
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-            if(player.trocarmapa==1)
+
+        //se morrer, voltar tudo ao default(voltar ao menu inicial)
+        if(saveatual.vidas==0 && Paused == false){
+            saveatual.fase = 0;
+            Paused = !Paused;
+        }
+
+
+        if(saveatual.trocarmapa==1)
             {
-                player.trocarmapa = 0;
-                leMapa(nome_mapas[player.fase-1], &mapas[player.fase-1], &player);
+                saveatual.trocarmapa = 0;
+                leMapa(nome_mapas[saveatual.fase], &mapas[saveatual.fase], &player);
+                respawna(&player,1);
             }
+        if(Paused){
+                if(saveatual.menustate==1){SetExitKey(KEY_Q);}
+                else{SetExitKey(0);}
+                DrawTexture(texturas.BG1, 0, 0, WHITE);
+
+                if(saveatual.fase>0){
+                renderiza_mapa(&mapas[saveatual.fase],&player,texturas);
+                spritesPlayer(&player, texturas);
+                HUD(&mapas[saveatual.fase],&player,texturas,&saveatual);
+                }
+
+
+                GUI(texturas,&saveatual,&save1,&save2,&save3,&player);
+            }
+
         if(!Paused){
             //funcoes principais
-            renderiza_mapa(&mapas[player.fase-1],&player,texturas);
+
+            SetExitKey(0);
+
+            renderiza_mapa(&mapas[saveatual.fase],&player,texturas);
+
             spritesPlayer(&player, texturas);
-            checacolisoes(&mapas[player.fase-1],&player);
-            hud(&mapas[player.fase-1],&player,texturas);
+
+            checacolisoes(&mapas[saveatual.fase],&player,&saveatual);
+
+            HUD(&mapas[saveatual.fase],&player,texturas,&saveatual);
+
+
             movimento(&player);
             gravidade(&player);
             atualizaPlayer(&player);
             respawna(&player,0);
             //ataques(&player,texturas);
             }
-        if(Paused){
-                renderiza_mapa(&mapas[player.fase-1],&player,texturas);
-                hud(&mapas[player.fase-1],&player,texturas);
-                spritesPlayer(&player, texturas);
-
-                DrawText("Pause",420,140,60,MAROON);}
 
         EndDrawing();
 
